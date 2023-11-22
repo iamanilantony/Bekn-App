a77681e8ab6b2d1605113e472e5afe484e7ea4c3
Anil Antony <anilantony3134@gmail.com>
2023-11-22 22:17:32 +0530

    chore: init setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
-import Image from "next/image";
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 
 export default function Home() {
   return (
-    <main>
-      <div>Hi</div>
-    </main>
+    <MaxWidthWrapper>
+      <div className="py-20 flex flex-col text-center item-center max-w-3xl mx-auto">
+        <h1 className="text-5xl font-bold">
+          Your Marketplace for highquality{" "}
+          <span className="text-blue-500">digital assets</span>
+        </h1>
+      </div>
+    </MaxWidthWrapper>
   );
 }
